bd1860df7a4e1eca545dcf5c3e458cbf3e454a8d
AugustoDundes <gutodundes@hotmail.com>
2024-11-23 11:21:20 -0300

    teste

--- projetobd/resources/views/dashboard.blade.php
+++
@@ -1,9 +0,0 @@
-<x-app-layout>
-
-    <h5 class="mt-3">Bem vindo ao sistema de controle de frota!</h5>
-
-
-    <div class="d-flex justify-content-center">
-        <div id="piechart" style="width: 900px; height: 500px;"></div>
-    </div>
-</x-app-layout>
